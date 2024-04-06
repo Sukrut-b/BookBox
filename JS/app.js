e3cc11822bff946ecce5d66899a833e86dbe1c35
Sukrut-b <96252506+Sukrut-b@users.noreply.github.com>
2024-04-07 01:58:18 +0530

    y

--- JS/app.js
+++ JS/app.js
@@ -38,7 +38,7 @@ window.addEventListener("click", (el) => {
           return el.target.parentElement.parentElement.children[1].children[2].children[0].innerText;
         }
       })(),
-      category: book.categories.map(category => category.name) || ["Uncategorized"] // Add category here
+      category: book.categorie.map(category => category.name) || ["Uncategorized"] // Add category here
     };
 
     dataState.push(data);
